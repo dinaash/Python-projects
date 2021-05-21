8e7fbf3b33812018c61b5ecd19b05bf65f6246ed
dinaash <deena.ashimova@gmail.com>
2021-05-21 08:46:37 +0200

    Add files via upload
    The data mining algorithm applied in this submission is a multinomial Bayesian classifier. This algorithm makes an assumption that all the predictor variables in the data set are mutually independent (Chauhan, 2018). For the given data set, the predictor variables are words in Reddit submissions’ titles. As the article explains, the algorithm is based on the relationship between classes (Reddit subreddit names, in this assignment) and features (tfidf of words in the submission, in this assignment) defined by the Bayes theorem.  
    
    Probability of a text with features x1...xn belonging to a class C is the probability of having these features given class C multiplied by the probability of belonging to class C divided by the probability of having the given features, as summarized in the article. 
     In the attached Python script, the submissions for two different subreddits were pre-processed for model fitting according to a tutorial in Smetanin (2018). The multinomial Bayesian classifier used in the tutorial estimates the probability of a feature i appearing in a sample that belongs to the class C as a smoothed version of relative frequency counting: the number of times feature i appears in a sample of class k in the training set divided by the total count of all features for class C. 
    
    Chauhan, G. (2018) All about Naive Bayes, towards data science, Oct 8, 2018, available: https://towardsdatascience.com/all-about-naive-bayes-8e13cef044cf [accessed 6 April 2021]
    
    Smetanin, S (2018) Sentiment Analysis of Tweets using Multinomial Naive Bayes, towards data science, Sep 1, available: https://towardsdatascience.com/sentiment-analysis-of-tweets-using-multinomial-naive-bayes-1009ed24276b  [accessed 6 April 2021]

--- main.py
+++ main.py
@@ -1,138 +1,187 @@
-import time
-import requests
-from datetime import datetime
-import pickle
+import creds
+import praw
+import pprint as pp
 import pandas as pd
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk import tokenize
-import nltk
-#nltk.download("vader_lexicon")
-import matplotlib.pyplot as plt
-#nltk.download('punkt')
+from praw.models import MoreComments
+from operator import attrgetter
+import pickle
+import numpy as np
+import re
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
 
+reddit = praw.Reddit(client_id = creds.client_id,\
+                     client_secret = creds.client_secret,\
+                     user_agent = creds.user_agent, \
+                     username = creds.username, \
+                     password = creds.password)
+
+class commentClass:
+    def __init__(self):
+        self._title = ""
+        self._comments = []
+        self._created = 0
+        self._score = 0
+        self._id = 0
+
+    def __len__(self):
+        return len(self._comments)
+
+    def __getitem__(self, position):
+        return self._comments[position]
+
+class masterClass:
+    def __init__(self):
+        self._class_list = []
+        self._index = 0
+
+    def __len__(self):
+        return len(self._class_list)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index >= len(self._class_list):
+            raise StopIteration
+        index = self._index
+        self._index += 1
+        return self._class_list[index]
+
+    def sort_length(self, attri_key):
+        sorted_comments = sorted(self._class_list, key = attrgetter(attri_key))
+        return sorted_comments
+    
 def main():
+
+    # Load the first 100 hot submissions from two different subreddits
+    firstClass = createClass("coronavirus",100)
+    secondClass = createClass("luxembourg",100)
+
+    #Create a list of the loaded submissions' titles
+    raw_data = []
+    #Fill the list with coronavirus submission titles, then luxembourg
+    for cls in firstClass:
+        raw_data.append(cls._title)
+    for cls in secondClass:
+        raw_data.append(cls._title)
+
+    #Create labels for each of the titles
+    #As the first 100 titles in the list are from the coronavirus subreddit
+        #the first 100 lables in the labels list are coronavirus
+    labels = ["coronavirus"]*100 + ["luxembourg"]*100
+    #Process the loaded titles using the pre-defined function
+    data = [preprocess_text(t) for t in raw_data]
+    #Create the processing stages that specify the type of vector representation
+    #and the desired classifier
+
+    text_clf = Pipeline([('vect', CountVectorizer()),
+                     ('tfidf', TfidfTransformer()),
+                     ('clf', MultinomialNB())])
+    #List parameters for the text vectorizing methods and classifier method
+    tuned_parameters = {
+    'vect__ngram_range': [(1, 1), (1, 2), (2, 2)],
+    'tfidf__use_idf': (True, False),
+    'tfidf__norm': ('l1', 'l2'),
+    'clf__alpha': [1, 1e-1, 1e-2]
+    }
+    
+    #Split the data into train set and test set
+    x_train, x_test, y_train, y_test = train_test_split(
+        data,
+        labels,
+        test_size=0.33,
+        random_state=42)
+    #Fit the multinomial Bayesian classifier with different parameters and 
+    #print the model accuracy score with best performing combination of parameter
+    clf = GridSearchCV(text_clf, tuned_parameters, cv=10, scoring='f1')
+    clf.fit(x_train, y_train)
+    print(classification_report(y_test, clf.predict(x_test), digits=4))
+
+    
+
+def preprocess_text (text):
+    text = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL', text)
+    text = re.sub('@[^\s]+','USER', text)
+    text = text.lower().replace("ё", "е")
+    text = re.sub('[^a-zA-Zа-яА-Я1-9]+', ' ', text)
+    text = re.sub(' +',' ', text)
+    return text.strip() 
+
+       
+def createClass(name, sample_size):
+    sub_ids = lookAtSubreddit(name,sample_size)
+    maClass = masterClass()
+    for id in list(sub_ids)[:sample_size]:
+        cls = commentClass()
+        cls._title = id.title
+        cls._comments = harvestCommentReplies(id)
+        cls._created = id.created_utc
+        cls._score = id.score
+        cls._id = id.id
+        maClass._class_list.append(cls)
+    return maClass
+
+
+    
+
+def harvestCommentReplies(id):
+    
+    sub = reddit.submission(id = id)
+    sub.comments.replace_more(limit = None)
+    comment_queue = sub.comments[:]
+    all_comments = []
+
+    while comment_queue:
+        comment = comment_queue.pop(0)
+        all_comments.append(comment.body)
+        comment_queue.extend(comment.replies)
+
+       
+    return all_comments
+
+
+def lookAtSubreddit(name,sample_size):
+    sub = reddit.subreddit(name).hot(limit = sample_size)
+    return sub
+
+    
+
+if __name__ == "__main__":
+    main()
+    
    
 
 
-    df_2019 = loadData(1554073200,1554678000, "2019_data.pkl")
-    print(df_2019.head())
-    print(df_2019.tail())
-    
-    df_2021 = loadData(1617231600,1617836400,"2021_data.pkl")
-    print(df_2021.head())
-    print(df_2021.tail())
-
-    limit = len(df_2019.days.value_counts().tolist())
-    
-    x = list(range(1,9))
-    fig, ax = plt.subplots()
-    ax.plot(x, totalSummation(limit, df_2019), color = "black", label = "before")
-    ax.plot(x, totalSummation(limit, df_2021), color = "red", label = "later")
-    leg = ax.legend()
-    plt.show()
 
 
-def totalSummation(limit, df):
 
-    limit = len(df.days.value_counts().tolist())
-    total_summation = []
-    for i in range (1, limit+1):
-        oneTotal = df['polar_sent'][df['days']==i].sum()
-        total_summation.append(oneTotal/df["days"].value_counts().tolist()[i-1])
-    return total_summation 
 
-def loadData(start,end,file_name):
-    
-    url = "https:://api.pushshift.io/reddit/search"
-    list_class = pullshiftpull(start,end)
-    picklefile = open(file_name,"wb")
-    pickle.dump(list_class, picklefile)
-    picklefile = open(file_name,"rb")
-    data_class = pickle.load(picklefile)
 
-    dates = []
-    text = []
-    for cls in data_class:
-        dates.append(cls.created_utc)
-        text.append(cls.body)
-    df = pd.DataFrame({"dates":dates,"body":text})
-    df['days']=df.dates.apply(extractDay)
-    df['polar_sent']=df.body.apply(determineSentimentPolarity)
 
-    return df
+
     
 
 
-def determineSentimentPolarity(row):
-
-    
-    row_score = 0
-    sid = SentimentIntensityAnalyzer()
-
-    for sentence in tokenize.sent_tokenize(row):
-        row_score += sid.polarity_scores(sentence)["compound"]
-  
-    return row_score
-    
-
-def extractDay(row):
-    dt = datetime.fromtimestamp(row)
-    return dt.day
-
-class redditSubmission:
-    def __init__(self):
-        self.body = ""
-        self.created_utc = ""
-
-def pullshiftpull(start_stamp,end_stamp):
-    
-    subreddit = "ireland"
-
-    url = "https://api.pushshift.io/reddit/search/?limit=100&after={}&before={}&subreddit={}"
-    list_class = []
-
-    while start_stamp < end_stamp:
-        
-        time.sleep(1) 
-
-        update_url = url.format(start_stamp, end_stamp, subreddit)
-
-        
-        json = requests.get(update_url)
-        print(json.status_code)
-
-        if json.status_code !=200:
-            continue
-        else:
-            json_data = json.json()
-            if "data" not in json_data:
-                break
-            else:
-                json_data = json_data['data']
-                if len(json_data)==0:
-                    print("no more data")
-                    break
-                try:
-                    start_stamp = json_data[-1]['created_utc']
-                except:
-                    start_stamp = end_stamp
-                list_class = processJsonData(json_data, list_class)
-                print(datetime.fromtimestamp(start_stamp))
-                print()
-        
-        
-    return list_class
-        
-
-def processJsonData(json_data, list_class):
-    for item in json_data:
-        redCls = redditSubmission()
-        redCls.body = item['body']
-        redCls.created_utc = item['created_utc']
-        list_class.append(redCls)
-    return list_class
 
 
 
-if __name__== "__main__":
-    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
